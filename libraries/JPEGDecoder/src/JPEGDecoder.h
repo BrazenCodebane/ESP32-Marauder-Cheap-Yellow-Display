e2d0e3a83084c6b5894bab10b90cab92a0737078
fmbellomy <fmbellomy@quantumgarbage.com>
2024-10-04 20:41:40 -0500

    file restructuring, addition of pio libraries to satisfy compiler requirements

--- libraries/JPEGDecoder/src/JPEGDecoder.h
+++ libraries/JPEGDecoder/src/JPEGDecoder.h
@@ -12,70 +12,65 @@ https://github.com/Bodmer/JPEGDecoder
 */
 
 #ifndef JPEGDECODER_H
-  #define JPEGDECODER_H
+#define JPEGDECODER_H
 
-  #ifndef JPEGDECODER_SETUP_LOADED //  Lets PlatformIO users define settings in
-                                   //  platformio.ini
-    #include "User_Config.h"
-  #endif // JPEGDECODER_SETUP_LOADED
+#ifndef JPEGDECODER_SETUP_LOADED //  Lets PlatformIO users define settings in
+                                 //  platformio.ini
+#include "User_Config.h"
+#endif // JPEGDECODER_SETUP_LOADED
 
-  #include "Arduino.h"
+#include "Arduino.h"
 
-  #ifdef __AVR__
-    #include <avr/pgmspace.h>
-    #undef PROGMEM
-    #define PROGMEM  __attribute__((section(".fini2")))
-  #endif
+#ifdef __AVR__
+#include <avr/pgmspace.h>
+#undef PROGMEM
+#define PROGMEM __attribute__((section(".fini2")))
+#endif
 
-  #ifdef ESP32  // SDFAT library not compatible with ESP32
-    //#undef LOAD_SD_LIBRARY
-    #undef LOAD_SDFAT_LIBRARY
-  #endif
+#ifdef ESP32 // SDFAT library not compatible with ESP32
+// #undef LOAD_SD_LIBRARY
+#undef LOAD_SDFAT_LIBRARY
+#endif
 
-  // New ESP8266 board package uses ARDUINO_ARCH_ESP8266
-  // old package defined ESP8266
-  #if defined (ESP8266)
-    #ifndef ARDUINO_ARCH_ESP8266
-      #define ARDUINO_ARCH_ESP8266
-    #endif
-  #endif
+// New ESP8266 board package uses ARDUINO_ARCH_ESP8266
+// old package defined ESP8266
+#if defined(ESP8266)
+#ifndef ARDUINO_ARCH_ESP8266
+#define ARDUINO_ARCH_ESP8266
+#endif
+#endif
 
-  #if defined (ARDUINO_ARCH_ESP8266) || defined (ESP32)
-    #define LOAD_FLASH_FS
-    #include <pgmspace.h>
-    #include <FS.h>
-    #include <LittleFS.h>
-    //#define SPIFFS LittleFS
-  #elif defined (ARDUINO_ARCH_RP2040)
-    #define LOAD_FLASH_FS
-    #include <FS.h>
-    #include <LittleFS.h>
-    #define SPIFFS LittleFS
-    #define TJPGD_LOAD_FFS
-  #endif
+#if defined(ARDUINO_ARCH_ESP8266) || defined(ESP32)
+#define LOAD_FLASH_FS
+#include <FS.h>
+#include <LITTLEFS.h>
+#include <pgmspace.h>
+// #define SPIFFS LittleFS
+#elif defined(ARDUINO_ARCH_RP2040)
+#define LOAD_FLASH_FS
+#include <FS.h>
+#include <LITTLEFS.h>
+#define SPIFFS LittleFS
+#define TJPGD_LOAD_FFS
+#endif
 
-  #if defined (LOAD_SD_LIBRARY) || defined (LOAD_SDFAT_LIBRARY)
-    #ifdef LOAD_SDFAT_LIBRARY
-      #include <SdFat.h> // Alternative where we might need to bit bash the SPI
-    #else
-      #include <SD.h>    // Default
-    #endif
-  #endif
+#if defined(LOAD_SD_LIBRARY) || defined(LOAD_SDFAT_LIBRARY)
+#ifdef LOAD_SDFAT_LIBRARY
+#include <SdFat.h> // Alternative where we might need to bit bash the SPI
+#else
+#include <SD.h> // Default
+#endif
+#endif
 
-  
 #include "picojpeg.h"
 
-enum {
-  JPEG_ARRAY = 0,
-  JPEG_FS_FILE,
-  JPEG_SD_FILE
-};
+enum { JPEG_ARRAY = 0, JPEG_FS_FILE, JPEG_SD_FILE };
 
-//#define DEBUG
+// #define DEBUG
 
 //------------------------------------------------------------------------------
 #ifndef jpg_min
-  #define jpg_min(a,b) (((a) < (b)) ? (a) : (b))
+#define jpg_min(a, b) (((a) < (b)) ? (a) : (b))
 #endif
 
 //------------------------------------------------------------------------------
@@ -86,7 +81,7 @@ typedef unsigned int uint;
 class JPEGDecoder {
 
 private:
-#if defined (LOAD_SD_LIBRARY) || defined (LOAD_SDFAT_LIBRARY)
+#if defined(LOAD_SD_LIBRARY) || defined(LOAD_SDFAT_LIBRARY)
   File g_pInFileSd;
 #endif
 #ifdef LOAD_FLASH_FS
@@ -94,7 +89,7 @@ private:
 #endif
   pjpeg_scan_type_t scan_type;
   pjpeg_image_info_t image_info;
-  
+
   int is_available;
   int mcu_x;
   int mcu_y;
@@ -105,14 +100,18 @@ private:
   uint row_blocks_per_mcu, col_blocks_per_mcu;
   uint8 status;
   uint8 jpg_source = 0;
-  uint8_t* jpg_data;
-  
-  static uint8 pjpeg_callback(unsigned char* pBuf, unsigned char buf_size, unsigned char *pBytes_actually_read, void *pCallback_data);
-  uint8 pjpeg_need_bytes_callback(unsigned char* pBuf, unsigned char buf_size, unsigned char *pBytes_actually_read, void *pCallback_data);
+  uint8_t *jpg_data;
+
+  static uint8 pjpeg_callback(unsigned char *pBuf, unsigned char buf_size,
+                              unsigned char *pBytes_actually_read,
+                              void *pCallback_data);
+  uint8 pjpeg_need_bytes_callback(unsigned char *pBuf, unsigned char buf_size,
+                                  unsigned char *pBytes_actually_read,
+                                  void *pCallback_data);
   int decode_mcu(void);
   int decodeCommon(void);
-public:
 
+public:
   uint16_t *pImage;
   JPEGDecoder *thisPtr;
 
@@ -126,32 +125,31 @@ public:
   int MCUHeight;
   int MCUx;
   int MCUy;
-  
+
   JPEGDecoder();
   ~JPEGDecoder();
 
   int available(void);
   int read(void);
   int readSwappedBytes(void);
-  
-  int decodeFile (const char *pFilename);
-  int decodeFile (const String& pFilename);
-  
-#if defined (LOAD_SD_LIBRARY) || defined (LOAD_SDFAT_LIBRARY)
-  int decodeSdFile (const char *pFilename);
-  int decodeSdFile (const String& pFilename);
-  int decodeSdFile (File g_pInFile);
+
+  int decodeFile(const char *pFilename);
+  int decodeFile(const String &pFilename);
+
+#if defined(LOAD_SD_LIBRARY) || defined(LOAD_SDFAT_LIBRARY)
+  int decodeSdFile(const char *pFilename);
+  int decodeSdFile(const String &pFilename);
+  int decodeSdFile(File g_pInFile);
 #endif
 
 #ifdef LOAD_FLASH_FS
-  int decodeFsFile (const char *pFilename);
-  int decodeFsFile (const String& pFilename);
-  int decodeFsFile (fs::File g_pInFile);
+  int decodeFsFile(const char *pFilename);
+  int decodeFsFile(const String &pFilename);
+  int decodeFsFile(fs::File g_pInFile);
 #endif
 
-  int decodeArray(const uint8_t array[], uint32_t  array_size);
+  int decodeArray(const uint8_t array[], uint32_t array_size);
   void abort(void);
-
 };
 
 extern JPEGDecoder JpegDec;
